5550a59b6cca589c65795177aa23403357481b97
Soukaina111 <basmahajar@gmail.com>
2024-06-27 17:12:07 +0000

    ES6 2nd task

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,24 +1,37 @@
+// Define the HolbertonCourse class that can be exported
 class HolbertonCourse {
-	/*defining the constructor of the class */
+  // The constructor initializes the name, length, and students properties of the HolbertonCourse instance
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
     this.students = students;
   }
 
-  /*setter of name attribute */
+  /**
+   * Setter method for the name property
+   * @param {String} name - The name of the Holberton course
+   * @throws {TypeError} - If the name is not a string
+   */
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
-  /*getter of name attribute */
+
+  /**
+   * Getter method for the name property
+   * @returns {String} - The name of the Holberton course
+   */
   get name() {
     return this._name;
   }
 
-  /*setter and getter of length attribute*/
+  /**
+   * Setter method for the length property
+   * @param {Number} length - The length of the Holberton course in weeks
+   * @throws {TypeError} - If the length is not a number
+   */
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
@@ -26,12 +39,18 @@ class HolbertonCourse {
     this._length = length;
   }
 
+  /**
+   * Getter method for the length property
+   * @returns {Number} - The length of the Holberton course in weeks
+   */
   get length() {
     return this._length;
   }
 
   /**
-   *setter and getter of students attribute
+   * Setter method for the students property
+   * @param {Array} students - An array of students enrolled in the Holberton course
+   * @throws {TypeError} - If the students argument is not an array
    */
   set students(students) {
     if (students instanceof Array) {
@@ -41,9 +60,14 @@ class HolbertonCourse {
     }
   }
 
+  /**
+   * Getter method for the students property
+   * @returns {Array} - An array of students enrolled in the Holberton course
+   */
   get students() {
     return this._students;
   }
 }
- /*this one is to make it usalble for other codes*/ 
+
+// Export the HolbertonCourse class as the default export
 export default HolbertonCourse;
