d8d4a48c71140546310e2a9177cd8975ab1e36f5
Soukaina111 <basmahajar@gmail.com>
2024-07-01 15:44:30 +0000

    fix 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,6 @@
 const getStudentIdsSum = (students) => {
-  // Use the reduce() method to iterate through the students array and sum up their 'id' values
+  /*Use the reduce() method to iterate through the
+   * students array and sum up their 'id' values*/
   return students.reduce((accum, now) => accum + now.id, 0);
 };
 
