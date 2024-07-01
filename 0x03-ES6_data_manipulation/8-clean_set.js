3f89c9237c21a7a9532c00e6234e58174a7a2463
Soukaina111 <basmahajar@gmail.com>
2024-07-01 15:30:09 +0000

    8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,22 @@
+const cleanSet = (set, startString) => {
+  // Initialize an empty array to store the cleaned values
+  const strg = [];
+
+  // Check if the startString is an empty string or not a string
+  if (startString === '' || typeof startString !== 'string') {
+    // If the startString is invalid, return an empty string
+    return '';
+  }
+
+  // Iterate through the set and add the values that start with the startString to the strings array
+  set.forEach((str) => {
+    if (typeof str === 'string' && str.startsWith(startString)) {
+      strg.push(s.slice(startString.length));
+    }
+  });
+
+  // Join the cleaned values in the strings array with a hyphen and return the result
+  return strg.join('-');
+};
+
+export default cleanSet;
