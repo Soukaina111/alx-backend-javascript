58a288370c06778179761240080de83c40f2d66c
Soukaina111 <basmahajar@gmail.com>
2024-07-01 16:01:35 +0000

    fix 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,14 +1,16 @@
 const groceriesList = () => {
   // Create a new Map to store the grocery items and their quantities
-  const map = new Map();
+  const carte = new Map();
 
   // Add items and their quantities to the grocery list Map
-  map.set('Apples', 10);
-  map.set('Tomatoes', 10);
-  map.set('Pasta', 1);
-  map.set('Rice', 1);
-  map.set('Banana', 5);
+  carte.set('Apples', 10);
+  carte.set('Tomatoes', 10);
+  carte.set('Pasta', 1);
+  carte.set('Rice', 1);
+  carte.set('Banana', 5);
 
   // Return the grocery list Map
-  return map;
+  return carte;
 };
+
+export default groceriesList;
