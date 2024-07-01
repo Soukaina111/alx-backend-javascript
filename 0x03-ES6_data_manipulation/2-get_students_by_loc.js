29ac7712f5b86186c2232ba940c3961ef5f64565
Soukaina111 <basmahajar@gmail.com>
2024-07-01 15:40:46 +0000

    fix 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,5 @@
 function getStudentsByLocation(students, city) {
+  // Filter the students array to only include students with the specified city in their 'location' property
   return students.filter((student) => student.location === city);
 }
 
