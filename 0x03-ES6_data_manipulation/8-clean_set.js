d06f5fe1dd98de5aabdb20c3016375953a44b4d7
Soukaina111 <basmahajar@gmail.com>
2024-07-01 15:57:30 +0000

    fix 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,22 +1,22 @@
 const cleanSet = (set, startString) => {
   // Initialize an empty array to store the cleaned values
-  const strg = [];
+  const strings = [];
 
   // Check if the startString is an empty string or not a string
-  if (startString === '' || typeof startString !== 'strg') {
+  if (startString === '' || typeof startString !== 'string') {
     // If the startString is invalid, return an empty string
     return '';
   }
 
   // Iterate through the set and add the values that start with the startString to the strings array
-  set.forEach((str) => {
-    if (typeof str === 'strg' && str.startsWith(startString)) {
-      strg.push(s.slice(startString.length));
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
     }
   });
 
   // Join the cleaned values in the strings array with a hyphen and return the result
-  return strg.join('-');
+  return strings.join('-');
 };
 
 export default cleanSet;
