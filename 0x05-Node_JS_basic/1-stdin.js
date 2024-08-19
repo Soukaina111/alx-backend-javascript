547b5dca97f1c0c0dd066a547c51fe545fe9e4dd
Soukaina111 <basmahajar@gmail.com>
2024-08-19 17:14:02 +0000

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+// Write a message to the standard output (console)
+// // asking the user for their name
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// Listen for 'data' events on the standard input (user input)
+// // When data is received, write a message back to the standard output
+// // containing the user's name
+process.stdin.on('data', (data) => {
+	  process.stdout.write(`Your name is: ${data}`);
+});
+process.stdin.on('end', () => {
+	  process.stdout.write('This important software is now closing\n');
+});
