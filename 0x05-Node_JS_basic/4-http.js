22873f39d48c83e22714222157ba954f27121a5e
Soukaina111 <basmahajar@gmail.com>
2024-08-20 19:53:23 +0100

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,18 +4,16 @@ const PORT = 1245;
 const HOST = 'localhost';
 const app = http.createServer();
 
-
 app.on('request', (_, res) => {
-	  const responseText = 'Hello Holberton School!';
-
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.setHeader('Content-Length', responseText.length);
-	  res.statusCode = 200;
-	  res.write(Buffer.from(responseText));
+  const responseText = 'Hello Holberton School!';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
 app.listen(PORT, HOST, () => {
-	  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
-	module.exports = app;
+module.exports = app;
